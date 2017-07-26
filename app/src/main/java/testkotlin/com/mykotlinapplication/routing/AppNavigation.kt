610c7447d01e78d099fa2fe5203f97c37feca437
Bernat Gomez <bernat.gomez@mobetia.com>
2017-07-26 15:47:26 +0200

    Add navigation to detail screen

--- app/src/main/java/testkotlin/com/mykotlinapplication/routing/AppNavigation.kt
+++ app/src/main/java/testkotlin/com/mykotlinapplication/routing/AppNavigation.kt
@@ -1,8 +1,16 @@
 package testkotlin.com.mykotlinapplication.routing
 
 import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.ActivityOptionsCompat
+import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import roberto.com.retrofitapisample.models.Character
 import testkotlin.com.mykotlinapplication.view.activity.ActivityWithToolbar
+import testkotlin.com.mykotlinapplication.view.activity.DetailActivity
+import testkotlin.com.mykotlinapplication.view.fragments.DetailFragment
+import testkotlin.com.mykotlinapplication.viewmodels.CharacterVM
 
 /**
  * Created by RobertoMiranda on 17/05/2017.
@@ -11,12 +19,33 @@ import testkotlin.com.mykotlinapplication.view.activity.ActivityWithToolbar
 
 class AppNavigation{
 
-
     companion object {
         fun navigateToMarvelActivity(from: AppCompatActivity){
+            navigateTo(from, ActivityWithToolbar::class.java, null)
+        }
 
-            val intent = Intent(from, ActivityWithToolbar::class.java)
-            from.startActivity(intent)
+        fun navigateToDetailActivity(from : FragmentActivity, vm : CharacterVM) {
+            val b : Bundle = Bundle()
+
+            b.putSerializable(DetailFragment.CHARACTER_EXTRA, vm.model)
+
+            navigateTo(from, DetailActivity::class.java, b, vm.view)
+        }
+
+        private fun <T> navigateTo(from : FragmentActivity, target : Class<T>, b : Bundle?, sharedView : View? = null) {
+            var sharedData : Bundle
+            val intent = Intent(from, target)
+
+            if (b != null) {
+                intent.putExtras(b)
+            }
+
+            if (sharedView == null) {
+                from.startActivity(intent)
+            } else {
+                sharedData = ActivityOptionsCompat.makeSceneTransitionAnimation(from, sharedView, "list_detail_transition").toBundle()
+                from.startActivity(intent, sharedData)
+            }
         }
     }
 
